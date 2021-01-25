8ab2a581c797f4f6fe83970c82bdbaa460cbd9e8
David Castillla Henares <57146460+dchcobra@users.noreply.github.com>
2021-01-25 18:40:07 +0100

    nodejs

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const app = express()
+
+app.use(bodyParser.json())
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+)
+
+const port = process.env.PORT;
+
+app.use(function (req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Credentials", "true")
+    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT")
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+    )
+    next();
+})
+
+app.get("/", (req, res) => {
+    res.send('<form method="post">\
+    <input type="text" name="name">\
+    <input type="submit" value="Enviar">\
+    </form>');
+})
+
+app.post("/", (req, res) => {
+    res.send("Hola " + req.body.name);
+})
+
+app.listen(port, async () => {
+})
